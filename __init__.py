fd28066288f22e32a5a5758702a6686f4130f9e4
Marc Hoersken <info@marc-hoersken.de>
2012-03-31 20:22:23 +0200

    Fixed form generation for YAML 4.0.1

--- __init__.py
+++ __init__.py
@@ -16,12 +16,14 @@ def replace(match):
     else:
         _class = 'ym-fbox-text'
     if _errors:
-        _class += ' ym-fbox-error'
+        _class += ' ym-error'
         _content = _content.replace(_errors, strip_tags(_errors))
     return _content.replace('class="row"', 'class="%s"' % _class)
 
 def as_yaml(self):
     "Returns this form rendered as HTML <div>s."
-    return mark_safe(re.sub(r'<div class="row"><p class="ym-message">(.*?)</p>(.+?)type="(.+?)"(.+?)</div>', replace, self._html_output(u'<div class="row"><p class="ym-message">%(errors)s</p>%(label)s%(field)s%(help_text)s</div>', u'%s', '</div>', u'<small class="ym-message">%s</small>', False)))
+    _search = r'<div class="row"><strong class="ym-message">(.*?)</strong>(.+?)type="(.+?)"(.+?)</div>'
+    _output = u'<div class="row"><strong class="ym-message">%(errors)s</strong>%(label)s%(field)s%(help_text)s</div>'
+    return mark_safe(re.sub(_search, replace, self._html_output(_output, u'%s', '</div>', u'<small class="ym-message">%s</small>', False)))
 
 forms.ModelForm.as_yaml = as_yaml
