25d2b916d811e8aa5980be8b7cb4a45dcc69b433
Marc Hoersken <info@marc-hoersken.de>
2012-04-01 14:19:29 +0200

    Fixed select form field

--- __init__.py
+++ __init__.py
@@ -6,24 +6,30 @@ from django.utils.safestring import mark_safe
 def replace(match):
     _content = match.group(0)
     _errors = match.group(1)
-    _type = match.group(3)
+    _field = match.group(2)
+    if 'type=' in _field:
+        _type = re.search(r'type="(.+?)"', _field).group(1)
+    else:
+        _type = None
     if _type == 'checkbox' or _type == 'radio':
         _class = 'ym-fbox-check'
     elif _type == 'submit' or _type == 'button' or _type == 'reset':
         _class = 'ym-fbox-button'
-    elif _content.find('<select') > -1:
+    elif '<select' in _content:
         _class = 'ym-fbox-select'
     else:
         _class = 'ym-fbox-text'
     if _errors:
         _class += ' ym-error'
         _content = _content.replace(_errors, strip_tags(_errors))
+    else:
+        _content = _content.replace('<strong class="ym-message"></strong>', '')
     return _content.replace('class="row"', 'class="%s"' % _class)
 
 def as_yaml(self):
     "Returns this form rendered as HTML <div>s."
-    _search = r'<div class="row"><strong class="ym-message">(.*?)</strong>(.+?)type="(.+?)"(.+?)</div>'
+    _search = r'<div class="row"><strong class="ym-message">(.*?)</strong>(.+?)</div>'
     _output = u'<div class="row"><strong class="ym-message">%(errors)s</strong>%(label)s%(field)s%(help_text)s</div>'
-    return mark_safe(re.sub(_search, replace, self._html_output(_output, u'%s', '</div>', u'<small>%s</small>', False)))
+    return mark_safe(re.sub(_search, replace, self._html_output(_output, u'%s', '</div>', u'<small>%s</small>', False).replace('\n', '')))
 
 forms.ModelForm.as_yaml = as_yaml
