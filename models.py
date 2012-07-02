7bcd5116718fc6ee56cc7de84a9a67f873c3c229
Marc Hoersken <info@marc-hoersken.de>
2012-07-02 13:16:52 +0200

    Use LOGIN_URL and LOGOUT_URL settings instead of named views

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
 import django.template
 if not django.template.libraries.get('yamlcss.filters', None):
     django.template.add_to_builtins('yamlcss.filters')
+if not django.template.libraries.get('yamlcss.tags', None):
+    django.template.add_to_builtins('yamlcss.tags')
