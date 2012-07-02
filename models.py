efeddba98cd14b87fe5681dbc006f962ef3cd988
Marc Hoersken <info@marc-hoersken.de>
2012-07-02 13:30:07 +0200

    Moved custom template tags and filters into templatetags folder

--- models.py
+++ models.py
@@ -1,5 +1,1 @@
-import django.template
-if not django.template.libraries.get('yamlcss.filters', None):
-    django.template.add_to_builtins('yamlcss.filters')
-if not django.template.libraries.get('yamlcss.tags', None):
-    django.template.add_to_builtins('yamlcss.tags')
+from django.db import models
